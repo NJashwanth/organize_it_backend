b5261830b9d8511b04b7ec3b291331f7d8f5d394
JASHWANTH NEELA <jashwanthn066@gmail.com>
2025-05-10 21:33:23 -0400

    first commit

---
+++ src/main/java/com/jashwanth_projects/organize_it_backend/OrganizeItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jashwanth_projects.organize_it_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeItBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jashwanth_projects/organize_it_backend/OrganizeItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jashwanth_projects.organize_it_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizeItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
