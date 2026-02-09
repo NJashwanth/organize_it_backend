80310bdd4fcf4d3449266d0b6020fec0325565c5
JASHWANTH NEELA <jashwanthn066@gmail.com>
2026-02-08 23:43:04 -0500

    Add entrypoint comment

--- src/main/java/com/jashwanth_projects/organize_it_backend/OrganizeItBackendApplication.java
+++ src/main/java/com/jashwanth_projects/organize_it_backend/OrganizeItBackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class OrganizeItBackendApplication {
 
 	public static void main(String[] args) {
+		// Application entry point.
 		SpringApplication.run(OrganizeItBackendApplication.class, args);
 	}
 
